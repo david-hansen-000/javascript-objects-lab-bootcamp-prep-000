0533e37edad1efb89fa3553e2a0590e7c32bd17d
David Hansen <david.hansen.0000@gmail.com>
2018-08-31 15:31:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ var recipe = {
 function updateObjectWithKeyAndValue(obj, key, value) {
   var newObj = Object.assign({}, obj);
   newObj[key]=value;
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  //var newObj = Object.assign({}, obj);
+  obj[key]=value;
   return obj;
 }
 
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj);
+  delete obj[key];
+  return newObj;
+}
